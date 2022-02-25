d4fb25a8b5f06c4934cb38f248be04af9314bc04
enhopkin@gmail.com <enhopkin@gmail.com>
2022-02-25 10:12:14 -0700

    installed dependencies, and connected components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+import OrderForm from './components/Order Form'
+
 const App = () => {
+
+
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+      {<OrderForm />
+}
     </>
   );
 };
---
+++ src/components/Order Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const OrderForm= () => {
+
+
+
+    return(
+
+        <h1>JUST GETTING STARTED</h1>
+    )
+
+}
+
+
+export default OrderForm
